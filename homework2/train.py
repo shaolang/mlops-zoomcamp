15aee2e09442c3061bb2f944888964b9ca09c8f1
shaolang <shaolang@ymail.com>
2022-05-30 21:37:53 +0800

    Update train.py to autolog training

--- homework2/train.py
+++ homework2/train.py
@@ -1,9 +1,9 @@
-import argparse
-import os
-import pickle
-
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+import argparse
+import mlflow
+import os
+import pickle
 
 
 def load_pickle(filename: str):
@@ -12,15 +12,17 @@ def load_pickle(filename: str):
 
 
 def run(data_path):
+    with mlflow.start_run():
+        mlflow.sklearn.autolog()
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_valid)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_valid)
 
-    rmse = mean_squared_error(y_valid, y_pred, squared=False)
+        rmse = mean_squared_error(y_valid, y_pred, squared=False)
 
 
 if __name__ == '__main__':
