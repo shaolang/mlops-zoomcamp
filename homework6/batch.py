7cb8488406c18ebd2371f3ae0dffe7a8f702ca1b
shaolang <shaolang@ymail.com>
2022-07-24 20:39:37 +0800

    Complete question 1 of homework 6

--- homework6/batch.py
+++ homework6/batch.py
@@ -6,20 +6,35 @@ import pickle
 import pandas as pd
 
 
-year = int(sys.argv[1])
-month = int(sys.argv[2])
+def main(year, month):
+    year = int(sys.argv[1])
+    month = int(sys.argv[2])
 
-input_file = f'https://raw.githubusercontent.com/alexeygrigorev/datasets/master/nyc-tlc/fhv/fhv_tripdata_{year:04d}-{month:02d}.parquet'
-output_file = f's3://nyc-duration-prediction-alexey/taxi_type=fhv/year={year:04d}/month={month:02d}/predictions.parquet'
+    input_file = f'https://raw.githubusercontent.com/alexeygrigorev/datasets/master/nyc-tlc/fhv/fhv_tripdata_{year:04d}-{month:02d}.parquet'
+    # output_file = f's3://nyc-duration-prediction-alexey/taxi_type=fhv/year={year:04d}/month={month:02d}/predictions.parquet'
+    output_file = f'taxi_type=fhv_year={year:04d}_month={month:02d}.parquet'
+
+    with open('model.bin', 'rb') as f_in:
+        dv, lr = pickle.load(f_in)
+
+    categorical = ['PUlocationID', 'DOlocationID']
+    df = read_data(input_file, categorical)
+    df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = lr.predict(X_val)
+
+    print('predicted mean duration:', y_pred.mean())
+
+    df_result = pd.DataFrame()
+    df_result['ride_id'] = df['ride_id']
+    df_result['predicted_duration'] = y_pred
+
+    df_result.to_parquet(output_file, engine='pyarrow', index=False)
 
 
-with open('model.bin', 'rb') as f_in:
-    dv, lr = pickle.load(f_in)
-
-
-categorical = ['PUlocationID', 'DOlocationID']
-
-def read_data(filename):
+def read_data(filename, categorical):
     df = pd.read_parquet(filename)
 
     df['duration'] = df.dropOff_datetime - df.pickup_datetime
@@ -31,21 +46,5 @@ def read_data(filename):
 
     return df
 
-
-df = read_data(input_file)
-df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
-
-
-dicts = df[categorical].to_dict(orient='records')
-X_val = dv.transform(dicts)
-y_pred = lr.predict(X_val)
-
-
-print('predicted mean duration:', y_pred.mean())
-
-
-df_result = pd.DataFrame()
-df_result['ride_id'] = df['ride_id']
-df_result['predicted_duration'] = y_pred
-
-df_result.to_parquet(output_file, engine='pyarrow', index=False)
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])
